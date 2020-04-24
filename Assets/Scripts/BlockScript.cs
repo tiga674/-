e98e66101aeed22bdab88e252e31cb222c2bc4cd
taigateshima <tiga674@gmail.com>
2020-04-25 01:30:12 +0900

    初めてのコミット

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BlockScript : MonoBehaviour
+{
+    Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            renderer.material.color = Color.white;
+        }
+    }
+}
