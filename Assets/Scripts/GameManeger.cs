5608c101faae3523ceb7672ae5e66cadae668d6b
taigateshima <tiga674@gmail.com>
2020-04-29 20:23:41 +0900

    時間経過でゲームの難易度が上がる仕様を実装

--- Assets/Scripts/GameManeger.cs
+++ Assets/Scripts/GameManeger.cs
@@ -4,35 +4,57 @@ using UnityEngine;
 
 public class GameManeger : MonoBehaviour
 {
-    bool blockDrop1Flg = true;
+    bool blockDropFlg = true;
+    bool gameLevelUpReject = false;
+    float playTime = 0f;
+    GameObject dropBlock;
 
-    public float blockDrop1Interval;
+    public float blockDropInterval;
     public GameObject block1;
+    public GameObject block2;
 
     void Start()
     {
-        StartCoroutine(blockDrop1());
+        dropBlock = block1;
+        StartCoroutine(blockDrop());
     }
    
-    void Update()
+    void FixedUpdate()
     {
-        
+        //時間が経過する
+        playTime += Time.deltaTime;
+
+        //時間経過で難易度が上がる
+        if (playTime >= 50f)
+        {
+            if (gameLevelUpReject) { return; }
+
+            gameLevelUp();
+        }
     }
 
     //ブロックを一定の間隔で落とす
-    private IEnumerator blockDrop1()
+    private IEnumerator blockDrop()
     {
-        while (blockDrop1Flg)
+        while (blockDropFlg)
         {
-            yield return new WaitForSeconds(blockDrop1Interval);
+            yield return new WaitForSeconds(blockDropInterval);
 
             int x = Random.Range(-2, 4);
             int z = Random.Range(-2, 4);
 
             Instantiate(
-                block1,
+                dropBlock,
                 new Vector3(x, 20, z),
                 Quaternion.identity);
         }
+    }
+
+    //ブロックの落下スピードが上昇し、落下間隔が早くなる
+    void gameLevelUp()
+    {
+        dropBlock = block2;
+        blockDropInterval -= 2;
+        gameLevelUpReject = true;
     }
 }
